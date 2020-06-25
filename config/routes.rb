2a05c34d60413d573eb891f049812583767698b6
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-25 11:48:05 +1000

    created a show our each restaurant and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  resources :restaurants, only:[:index,:show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
